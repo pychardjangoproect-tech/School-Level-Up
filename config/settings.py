7749b8c1d5377cd31a3f9ab21c3cac8797bc4191
PK STORE <pychardjangoproect@gmail.com>
2026-09-22 13:41:04 +0600

    fix requirements

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-CHANGE-THIS-KEY-BEFOR
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
 
 CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='').split(',') if config('CSRF_TRUSTED_ORIGINS', default='') else []
 
